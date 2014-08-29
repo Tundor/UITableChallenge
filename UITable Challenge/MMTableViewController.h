8c7ed35b44b77df011a8e2c7535888f8deafce12
Manuel Müller <Manu@manuels-mbp.fritz.box>
2014-08-29 19:28:33 +0200

    first commit

---
+++ UITable Challenge/MMTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMTableViewController.h
+//  UITable Challenge
+//
+//  Created by Manuel Müller on 29.08.14.
+//  Copyright (c) 2014 Manuel Müller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMTableViewController : UITableViewController
+
+@end
