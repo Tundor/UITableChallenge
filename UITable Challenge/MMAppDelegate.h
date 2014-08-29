ec64a17c344bfb35449127e52bb48a548b86a620
Manuel Müller <Manu@manuels-mbp.fritz.box>
2014-08-29 19:00:47 +0200

    Initial Commit

---
+++ UITable Challenge/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  UITable Challenge
+//
+//  Created by Manuel Müller on 29.08.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
